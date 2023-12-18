10e1e19fc2f1a97aebefb0c260fd75b752716de5
Schedency <67629534+Schedency@users.noreply.github.com>
2023-12-18 17:14:02 +0100

    BlueChat.lua
    Signed-off-by: Schedency <67629534+Schedency@users.noreply.github.com>

---
+++ BlueChat.lua
@@ -0,0 +1,126 @@
+local Main = {}
+
+local Players = game:GetService("Players")
+local TextChatService = game:GetService("TextChatService")
+local MarketplaceService = game:GetService("MarketplaceService")
+
+local MessageProperties = Instance.new("TextChatMessageProperties")
+local BubbleProperties = Instance.new("BubbleChatMessageProperties")
+
+local errorMessage = "Problem with TextSource; source doesn't exist or is missing"
+
+function Main:CreateNewChatProperty(Type, Id, Rank, Tag, TagColor, BubbleTextColor, BubbleBackgroundColor, BubbleFont)
+
+	-- // User
+	if Type == "User" then
+
+		TextChatService.OnIncomingMessage = function(message: TextChatMessage)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if Player.UserId == Id then
+					MessageProperties.PrefixText = string.format(`<font color="{TagColor}">{Tag}</font> ` .. message.PrefixText)
+				else
+					return
+				end
+			else
+				warn(errorMessage)
+			end
+
+			return MessageProperties
+		end
+
+		TextChatService.OnBubbleAdded = function(message: TextChatMessage, adornee: Instance)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if Player.UserId == Id then
+					BubbleProperties.TextColor3 = Color3.fromHex(BubbleTextColor)
+					BubbleProperties.BackgroundColor3 = Color3.fromHex(BubbleBackgroundColor)
+					BubbleProperties.FontFace = Font.fromEnum(BubbleFont)
+				end
+			else
+				return warn(errorMessage)
+			end
+
+			return BubbleProperties
+		end
+
+		-- // Group
+	elseif Type == "Group" then
+
+		TextChatService.OnIncomingMessage = function(message: TextChatMessage)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if Rank == nil then
+					return warn("Rank is set to Nil")
+				else
+					if Player:GetRankInGroup(Id) == tonumber(Rank) then
+						MessageProperties.PrefixText = string.format(`<font color="{TagColor}">{Tag}</font> ` .. message.PrefixText)
+					end
+				end
+			else
+				return warn(errorMessage)
+			end
+
+			return MessageProperties
+		end
+
+		TextChatService.OnBubbleAdded = function(message: TextChatMessage, adornee: Instance)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if Rank == nil then
+					return warn("Rank is set to nil")
+				else
+					if Player:GetRankInGroup(Id) == tonumber(Rank) then
+						BubbleProperties.TextColor3 = Color3.fromHex(BubbleTextColor)
+						BubbleProperties.BackgroundColor3 = Color3.fromHex(BubbleBackgroundColor)
+						BubbleProperties.FontFace = Font.fromEnum(BubbleFont)
+					end
+				end
+			else
+				return warn(errorMessage)
+			end
+
+			return BubbleProperties
+		end
+
+		-- // Game Passes
+	elseif Type == "Pass" then
+
+		TextChatService.OnIncomingMessage = function(message: TextChatMessage)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if MarketplaceService:UserOwnsGamePassAsync(Player.UserId, Id) then
+					MessageProperties.PrefixText = string.format(`<font color="{TagColor}">{Tag}</font> ` .. message.PrefixText)
+				else
+					return
+				end
+			else
+				return warn(errorMessage)
+			end
+
+			return MessageProperties
+		end
+
+		TextChatService.OnBubbleAdded = function(message: TextChatMessage, adornee: Instance)
+			if message.TextSource then
+				local Player = Players:GetPlayerByUserId(message.TextSource.UserId)
+
+				if MarketplaceService:UserOwnsGamePassAsync(Player.UserId, Id) then
+					BubbleProperties.TextColor3 = Color3.fromHex(BubbleTextColor)
+					BubbleProperties.BackgroundColor3 = Color3.fromHex(BubbleBackgroundColor)
+					BubbleProperties.FontFace = Font.fromEnum(BubbleFont)
+				else
+					return
+				end
+			end
+		end
+
+	end
+end
+
+return Main
